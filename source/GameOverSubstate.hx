a43856e7d2428de46339965f1a944b0c54386a75
DJjoteeYT <95763645+DJjoteeYT@users.noreply.github.com>
2022-09-27 21:01:25 +0200

    Added gameoverstate and substate because I'm dumb

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -16,6 +16,9 @@ class GameOverSubstate extends MusicBeatSubstate
 
 	public function new(x:Float, y:Float)
 	{
+#if android
+	addVirtualPad(FULL, A_B);
+        #end
 		var daStage = PlayState.curStage;
 		var daBf:String = '';
 		switch (PlayState.SONG.player1)
